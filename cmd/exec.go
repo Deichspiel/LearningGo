9e09e7c4467652343061838f462a35a5bf117753
Deichspiel <Deichspiel@mailbox.org>
2020-05-15 11:19:18 +0200

    first checkin

---
+++ cmd/exec.go
@@ -0,0 +1,44 @@
+package cmd
+
+import(
+	"fmt"
+	"github.com/spf13/cobra"
+	"os"
+	// "strings"
+)
+
+var rootCmd = &cobra.Command {
+	Use: "LearningGo ",
+	Short: "My first go project",
+	Long: `Just a small CLI application. Read Atom feeds`,
+}
+
+var cmdLs = &cobra.Command {
+	Use: "ls",
+	Short: "List news",
+	Long: `List first 5 news`,
+	Run: func (cmd *cobra.Command, args []string)  {
+		list()
+	},
+}
+
+var cmdDescribe = &cobra.Command {
+	Use: "describe [id]",
+	Short: "Show details for an article",
+	Long: `Details for an article`,
+	Args: cobra.MinimumNArgs(1),
+	Run: func(cmd *cobra.Command, args []string) {
+		describe(args[0])
+	},
+}
+
+func Exec() {
+	rootCmd.AddCommand(cmdLs)
+	rootCmd.AddCommand(cmdDescribe)
+
+	err := rootCmd.Execute()
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+}
---
+++ main.go
@@ -0,0 +1,9 @@
+package main
+
+import (
+	"github.com/Deichspiel/LearningGo/cmd"
+)
+
+func main() {
+	cmd.Exec()
+}
